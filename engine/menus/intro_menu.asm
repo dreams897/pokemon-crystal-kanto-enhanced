8dfae55d3677ff66d3d14624e1f5cf7d005c76b7
dreams897 <dreams897@proton.me>
2024-03-01 14:53:46 -0800

    Added 1 Potion to player PC at start

--- engine/menus/intro_menu.asm
+++ engine/menus/intro_menu.asm
@@ -633,6 +633,8 @@ OakSpeech:
 	; farcall InitClock
 	call RotateFourPalettesLeft
 	call ClearTilemap
+	
+	call StartPCItem
 
 	ld de, MUSIC_ROUTE_30
 	call PlayMusic
@@ -782,6 +784,15 @@ OakText9:
 OakText10:
 	text_far _OakText10
 	text_end
+	
+StartPCItem:
+	ld a, POTION
+	ld [wCurItem], a
+	ld a, 1
+	ld [wItemQuantityChange], a
+	ld hl, wNumPCItems
+	call ReceiveItem
+	ret
 
 NamePlayer:
 	farcall MovePlayerPicRight
