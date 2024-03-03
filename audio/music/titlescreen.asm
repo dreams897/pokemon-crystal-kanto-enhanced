f3fbc9e95203e351e0b49a8444f6d5ff1ae056fd
dreams897 <dreams897@proton.me>
2024-03-02 18:15:03 -0800

    Add Stereo audio to titlescreen

--- audio/music/titlescreen.asm
+++ audio/music/titlescreen.asm
@@ -10,13 +10,16 @@ Music_TitleScreen_Ch1:
 	volume 7, 7
 	duty_cycle 3
 	vibrato 9, 3, 4
+	stereo_panning TRUE, FALSE
 	note_type 12, 12, 1
 	octave 2
 	note E_, 1
 	note G_, 1
 	note B_, 1
+	stereo_panning TRUE, TRUE
 	octave 3
 	note D_, 1
+	stereo_panning TRUE, FALSE
 	octave 2
 	note G_, 4
 	note G_, 6
@@ -32,10 +35,11 @@ Music_TitleScreen_Ch1:
 	note A_, 2
 	note A_, 2
 	note F#, 2
-.mainloop:
-	sound_call .sub1
-	sound_call .sub2
-	sound_call .sub1
+
+Music_RBYTitleScreen_branch_7e4e2::
+	sound_call Music_RBYTitleScreen_branch_7e541
+	sound_call Music_RBYTitleScreen_branch_7e54a
+	sound_call Music_RBYTitleScreen_branch_7e541
 	octave 3
 	note C_, 8
 	note_type 8, 12, 6
@@ -58,9 +62,9 @@ Music_TitleScreen_Ch1:
 	octave 3
 	note C_, 2
 	note D_, 2
-	sound_call .sub1
-	sound_call .sub2
-	sound_call .sub1
+	sound_call Music_RBYTitleScreen_branch_7e541
+	sound_call Music_RBYTitleScreen_branch_7e54a
+	sound_call Music_RBYTitleScreen_branch_7e541
 	note C_, 6
 	note C_, 6
 	note E_, 4
@@ -88,21 +92,21 @@ Music_TitleScreen_Ch1:
 	octave 4
 	note C_, 4
 	note C#, 4
-	sound_call .sub3
+	sound_call Music_RBYTitleScreen_branch_7e55c
 	note_type 8, 11, 4
 	octave 4
 	note C_, 4
 	note C_, 4
 	note C#, 4
-	sound_call .sub3
+	sound_call Music_RBYTitleScreen_branch_7e55c
 	note_type 8, 11, 2
 	octave 3
 	note E_, 4
 	note E_, 4
 	note C#, 4
-	sound_loop 0, .mainloop
+	sound_loop 0, Music_RBYTitleScreen_branch_7e4e2
 
-.sub1:
+Music_RBYTitleScreen_branch_7e541::
 	note_type 12, 12, 6
 	octave 3
 	note D_, 6
@@ -112,7 +116,7 @@ Music_TitleScreen_Ch1:
 	note D_, 8
 	sound_ret
 
-.sub2:
+Music_RBYTitleScreen_branch_7e54a::
 	note C_, 6
 	note F_, 6
 	note C_, 4
@@ -130,7 +134,7 @@ Music_TitleScreen_Ch1:
 	note C_, 4
 	sound_ret
 
-.sub3:
+Music_RBYTitleScreen_branch_7e55c::
 	note_type 12, 12, 1
 	note D_, 1
 	rest 1
@@ -160,13 +164,14 @@ Music_TitleScreen_Ch1:
 	sound_ret
 
 Music_TitleScreen_Ch2:
-	duty_cycle 1
 	vibrato 16, 4, 6
+	duty_cycle 1
 	note_type 12, 14, 1
 	octave 2
 	note G_, 1
 	note B_, 1
 	octave 3
+	stereo_panning TRUE, FALSE
 	note D_, 1
 	note F#, 1
 	note G_, 4
@@ -183,21 +188,26 @@ Music_TitleScreen_Ch2:
 	note F_, 2
 	note F_, 2
 	note F#, 2
-.mainloop:
+
+Music_RBYTitleScreen_branch_7e594::
 	vibrato 16, 4, 6
-	sound_call .sub1
+	sound_call Music_RBYTitleScreen_branch_7e622
+	stereo_panning TRUE, TRUE
 	octave 2
 	note A_, 4
 	note F_, 4
-	sound_call .sub2
+	sound_call Music_RBYTitleScreen_branch_7e62c
+	stereo_panning TRUE, FALSE
 	octave 2
 	note A_, 8
 	note B_, 16
-	sound_call .sub1
+	sound_call Music_RBYTitleScreen_branch_7e622
+	stereo_panning FALSE, TRUE
 	octave 2
 	note A_, 6
 	note F_, 2
 	note_type 8, 14, 7
+	stereo_panning TRUE, TRUE
 	octave 4
 	note C_, 4
 	octave 3
@@ -207,16 +217,19 @@ Music_TitleScreen_Ch2:
 	note_type 12, 14, 7
 	note D_, 8
 	note_type 12, 9, 5
+	stereo_panning TRUE, FALSE
 	octave 2
 	note D_, 6
 	note D_, 1
 	note F#, 1
 	note G_, 16
-	sound_call .sub1
+	sound_call Music_RBYTitleScreen_branch_7e622
+	stereo_panning TRUE, TRUE
 	octave 2
 	note A_, 2
 	note F_, 6
-	sound_call .sub2
+	sound_call Music_RBYTitleScreen_branch_7e62c
+	stereo_panning TRUE, FALSE
 	octave 3
 	note C_, 2
 	octave 2
@@ -224,13 +237,15 @@ Music_TitleScreen_Ch2:
 	note B_, 6
 	note G_, 2
 	note F_, 8
-	sound_call .sub1
+	sound_call Music_RBYTitleScreen_branch_7e622
 	note_type 8, 9, 5
+	stereo_panning FALSE, TRUE
 	octave 2
 	note G_, 4
 	note F_, 5
 	note A_, 3
 	note_type 8, 14, 6
+	stereo_panning TRUE, TRUE
 	octave 4
 	note F_, 4
 	note E_, 4
@@ -247,26 +262,32 @@ Music_TitleScreen_Ch2:
 	note A_, 8
 	duty_cycle 1
 	note_type 12, 14, 7
+	stereo_panning TRUE, FALSE
 	note A#, 6
 	note F_, 2
 	note F_, 8
 	octave 3
 	note D_, 8
+	stereo_panning TRUE, TRUE
 	octave 4
 	note A#, 4
 	note B_, 4
+	stereo_panning TRUE, FALSE
 	octave 5
 	note C_, 6
+	stereo_panning TRUE, TRUE
 	octave 4
 	note G_, 2
 	note G_, 8
+	stereo_panning TRUE, FALSE
 	octave 3
 	note E_, 8
 	note_type 12, 13, 7
+	stereo_panning TRUE, TRUE
 	octave 5
 	note C_, 4
 	note C#, 4
-	sound_call .sub3
+	sound_call Music_RBYTitleScreen_branch_7e636
 	rest 3
 	note D_, 1
 	rest 3
@@ -276,7 +297,7 @@ Music_TitleScreen_Ch2:
 	note C_, 4
 	note C_, 4
 	note C#, 4
-	sound_call .sub3
+	sound_call Music_RBYTitleScreen_branch_7e636
 	note D_, 1
 	rest 2
 	note D_, 1
@@ -286,21 +307,23 @@ Music_TitleScreen_Ch2:
 	octave 5
 	note C_, 4
 	note C_, 4
+	stereo_panning FALSE, TRUE
 	octave 4
 	note B_, 4
-	sound_loop 0, .mainloop
+	sound_loop 0, Music_RBYTitleScreen_branch_7e594
 
-.sub1:
+Music_RBYTitleScreen_branch_7e622::
 	note_type 12, 14, 7
 	octave 3
 	note G_, 6
 	note B_, 2
+	stereo_panning TRUE, TRUE
 	octave 4
 	note D_, 8
 	note_type 12, 9, 5
 	sound_ret
 
-.sub2:
+Music_RBYTitleScreen_branch_7e62c::
 	note_type 12, 14, 7
 	octave 4
 	note F_, 6
@@ -310,7 +333,7 @@ Music_TitleScreen_Ch2:
 	note_type 12, 9, 5
 	sound_ret
 
-.sub3:
+Music_RBYTitleScreen_branch_7e636::
 	note_type 12, 14, 1
 	note D_, 1
 	rest 2
@@ -325,6 +348,7 @@ Music_TitleScreen_Ch2:
 	sound_ret
 
 Music_TitleScreen_Ch3:
+	stereo_panning FALSE, TRUE
 	note_type 12, 1, 0
 	octave 3
 	note G_, 1
@@ -350,39 +374,41 @@ Music_TitleScreen_Ch3:
 	note F_, 2
 	note F_, 2
 	note A_, 2
-.mainloop:
-	sound_call .sub1
-	sound_call .sub2
-.loop1:
-	sound_call .sub1
-	sound_call .sub1
-	sound_call .sub1
-	sound_call .sub2
-	sound_loop 3, .loop1
-	sound_call .sub1
+
+Music_RBYTitleScreen_branch_7e65e::
+	sound_call Music_RBYTitleScreen_branch_7e6c9
+	sound_call Music_RBYTitleScreen_branch_7e6d0
+
+Music_RBYTitleScreen_branch_7e664::
+	sound_call Music_RBYTitleScreen_branch_7e6c9
+	sound_call Music_RBYTitleScreen_branch_7e6c9
+	sound_call Music_RBYTitleScreen_branch_7e6c9
+	sound_call Music_RBYTitleScreen_branch_7e6d0
+	sound_loop 3, Music_RBYTitleScreen_branch_7e664
+	sound_call Music_RBYTitleScreen_branch_7e6c9
 	note G_, 6
 	note D_, 3
 	note A_, 6
 	note F_, 3
 	note A_, 3
 	note F_, 3
-	sound_call .sub3
+	sound_call Music_RBYTitleScreen_branch_7e6d7
 	note A#, 3
 	note F_, 3
-	sound_call .sub3
+	sound_call Music_RBYTitleScreen_branch_7e6d7
 	note B_, 3
 	note G_, 3
-	sound_call .sub4
+	sound_call Music_RBYTitleScreen_branch_7e6dc
 	octave 4
 	note C_, 3
 	octave 3
 	note G_, 3
-	sound_call .sub4
+	sound_call Music_RBYTitleScreen_branch_7e6dc
 	octave 4
 	note C#, 3
 	octave 3
 	note A_, 3
-	sound_call .sub5
+	sound_call Music_RBYTitleScreen_branch_7e6e5
 	octave 5
 	pitch_slide 1, 4, D_
 	note D_, 4
@@ -399,7 +425,8 @@ Music_TitleScreen_Ch3:
 	note C_, 4
 	note C_, 4
 	note C#, 4
-	sound_call .sub5
+	sound_call Music_RBYTitleScreen_branch_7e6e5
+
 	octave 6
 	pitch_slide 1, 5, D_
 	note D_, 4
@@ -414,9 +441,9 @@ Music_TitleScreen_Ch3:
 	note C_, 4
 	octave 3
 	note B_, 4
-	sound_loop 0, .mainloop
+	sound_loop 0, Music_RBYTitleScreen_branch_7e65e
 
-.sub1:
+Music_RBYTitleScreen_branch_7e6c9::
 	note G_, 6
 	note D_, 3
 	note G_, 6
@@ -425,7 +452,7 @@ Music_TitleScreen_Ch3:
 	note D_, 3
 	sound_ret
 
-.sub2:
+Music_RBYTitleScreen_branch_7e6d0::
 	note F_, 6
 	note C_, 3
 	note F_, 6
@@ -434,25 +461,27 @@ Music_TitleScreen_Ch3:
 	note C_, 3
 	sound_ret
 
-.sub3:
+Music_RBYTitleScreen_branch_7e6d7::
 	note A#, 6
 	note F_, 3
 	note A#, 6
 	note F_, 3
 	sound_ret
 
-.sub4:
+Music_RBYTitleScreen_branch_7e6dc::
 	octave 4
 	note C_, 6
 	octave 3
 	note G_, 3
+	stereo_panning TRUE, TRUE
 	octave 4
 	note C_, 6
 	octave 3
 	note G_, 3
 	sound_ret
 
-.sub5:
+Music_RBYTitleScreen_branch_7e6e5::
+	stereo_panning FALSE, TRUE
 	note_type 12, 1, 0
 	octave 4
 	note D_, 1
@@ -460,6 +489,8 @@ Music_TitleScreen_Ch3:
 	sound_ret
 
 Music_TitleScreen_Ch4:
+	stereo_panning TRUE, FALSE
+	toggle_noise 0
 	drum_speed 6
 	rest 4
 	drum_note 3, 1
@@ -486,7 +517,8 @@ Music_TitleScreen_Ch4:
 	drum_note 3, 2
 	drum_note 2, 2
 	drum_note 1, 2
-.mainloop:
+
+Music_RBYTitleScreen_branch_7e716::
 	drum_speed 12
 	drum_note 2, 1
 	rest 3
@@ -496,8 +528,8 @@ Music_TitleScreen_Ch4:
 	drum_note 2, 1
 	drum_note 3, 1
 	rest 3
-	sound_call .sub1
-	sound_call .sub1
+	sound_call Music_RBYTitleScreen_branch_7e834
+	sound_call Music_RBYTitleScreen_branch_7e834
 	drum_note 2, 1
 	rest 3
 	drum_note 2, 1
@@ -508,7 +540,7 @@ Music_TitleScreen_Ch4:
 	rest 1
 	drum_note 3, 1
 	drum_note 2, 1
-	sound_call .sub2
+	sound_call Music_RBYTitleScreen_branch_7e842
 	drum_note 2, 1
 	rest 3
 	drum_note 2, 1
@@ -519,7 +551,7 @@ Music_TitleScreen_Ch4:
 	rest 1
 	drum_note 2, 1
 	rest 1
-	sound_call .sub2
+	sound_call Music_RBYTitleScreen_branch_7e842
 	drum_note 2, 1
 	rest 3
 	drum_note 2, 1
@@ -534,8 +566,8 @@ Music_TitleScreen_Ch4:
 	drum_note 4, 1
 	drum_note 4, 1
 	drum_speed 12
-	sound_call .sub1
-	sound_call .sub2
+	sound_call Music_RBYTitleScreen_branch_7e834
+	sound_call Music_RBYTitleScreen_branch_7e842
 	drum_note 2, 1
 	rest 3
 	drum_note 2, 1
@@ -554,8 +586,8 @@ Music_TitleScreen_Ch4:
 	rest 1
 	drum_note 3, 1
 	drum_note 2, 1
-	sound_call .sub1
-	sound_call .sub2
+	sound_call Music_RBYTitleScreen_branch_7e834
+	sound_call Music_RBYTitleScreen_branch_7e842
 	drum_note 2, 1
 	rest 3
 	drum_note 2, 1
@@ -651,9 +683,9 @@ Music_TitleScreen_Ch4:
 	drum_note 2, 4
 	drum_note 3, 4
 	drum_note 2, 4
-	sound_loop 0, .mainloop
+	sound_loop 0, Music_RBYTitleScreen_branch_7e716
 
-.sub1:
+Music_RBYTitleScreen_branch_7e834::
 	drum_note 2, 1
 	rest 3
 	drum_note 2, 1
@@ -664,7 +696,7 @@ Music_TitleScreen_Ch4:
 	rest 3
 	sound_ret
 
-.sub2:
+Music_RBYTitleScreen_branch_7e842::
 	drum_note 2, 1
 	rest 3
 	drum_note 2, 1
